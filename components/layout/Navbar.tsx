b9f21e417a3eaa98818d55519d77fca6d413095e
gerardonastri <gerardonastri06@gmail.com>
2024-11-29 15:01:57 +0100

    navbar text

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="max-w-[1700px] mx-auto px-[3.5rem] py-6">
       <div className="w-full flex items-center justify-between">
         <Link href="/">
-          <h1 className="text-3xl font-bold">Poliform</h1>
+          <h1 className="text-3xl font-bold">Contact Arredo</h1>
         </Link>
         <ul className="flex items-center gap-8">
           <li className="pb-2 hover:shadow-[inset_0_-2px_0_0_rgba(0,0,0,1)] transition-all ease-in duration-200">
