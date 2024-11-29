8ed8f2e3731e6821690820c5f4d517ef00dbca13
gerardonastri <gerardonastri06@gmail.com>
2024-11-29 13:44:22 +0100

    home page almost finished

--- components/CollectionGrid.tsx
+++ components/CollectionGrid.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import Masonry from "react-masonry-css";
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { ArrowUpRight } from "lucide-react";
 // import { Masonry } from "react-plock";
 
