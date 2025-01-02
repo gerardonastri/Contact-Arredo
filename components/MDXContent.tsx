1a344669c828feb26de7ff28a376c9c82e76fe9a
gerardonastri <gerardonastri06@gmail.com>
2025-01-02 11:54:05 +0100

    better

--- components/MDXContent.tsx
+++ components/MDXContent.tsx
@@ -2,8 +2,12 @@ import { JSX } from "react";
 import { highlight } from "sugar-high";
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 
-function Code({ children, ...props }: any) {
-  const codeHTML = highlight(children);
+function Code({ children, ...props }: React.HTMLAttributes<HTMLElement>) {
+  let codeHTML = "";
+
+  if (typeof children === "string") {
+    codeHTML = highlight(children);
+  }
   return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
 }
 
